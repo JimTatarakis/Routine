6441ebc519e4b70cf7caaad2f37c74e6c58e9ddb
Jim Tatarakis <jimtatarakis@gmail.com>
2019-06-25 20:51:13 -0700

    completed the schema and also completed the models

---
+++ models/Task.js
@@ -0,0 +1,28 @@
+module.exports = function(sequelize, DataTypes) {
+  var Task = sequelize.define("Task", {
+    name: {
+      type: DataTypes.STRING,
+      notNull: true,
+      validate: {
+        len: [1, 140]
+      }
+    },
+    completed: {
+      type: DataTypes.BOOLEAN,
+      defaultValue: false
+    },
+    assigned: {
+      type: DataTypes.STRING,
+      defaultValue: "open"
+    }
+  });
+  // Associate to User
+  Task.associate = function(models) {
+    Task.belongsTo(models.User, {
+      foreignKey: {
+        allowNull: true
+      }
+    });
+  };
+  return Task;
+};
