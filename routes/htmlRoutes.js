22cea795ad685cc54a3723b3d18de5e26f9d40d9
kellidragon <dragonkelli@gmail.com>
2019-06-26 22:03:34 -0700

    manager view tables created

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -20,27 +20,44 @@ module.exports = function(app) {
     });
   });
 
-  // Load example page and pass in an example by id
   app.get("/user", function(req, res) {
-    db.Example.findOne({ where: { id: req.params.id } }).then(function(
-      dbExample
-    ) {
-      res.render("example", {
-        example: dbExample
+    db.Example.findAll({}).then(function(dbExamples) {
+      res.render("user", {
+        msg: "Welcome!",
+        examples: dbExamples
       });
     });
   });
 
   app.get("/manager", function(req, res) {
-    db.Example.findOne({ where: { id: req.params.id } }).then(function(
-      dbExample
-    ) {
-      res.render("example", {
-        example: dbExample
+    db.Example.findAll({}).then(function(dbExamples) {
+      res.render("manager", {
+        msg: "Welcome!",
+        examples: dbExamples
       });
     });
   });
 
+  // Load example page and pass in an example by id
+  // app.get("/user", function(req, res) {
+  //   db.Example.findOne({ where: { id: req.params.id } }).then(function(
+  //     dbExample
+  //   ) {
+  //     res.render("user", {
+  //       example: dbExample
+  //     });
+  //   });
+  // });
+
+  // app.get("/manager", function(req, res) {
+  //   db.Example.findOne({ where: { id: req.params.id } }).then(function(
+  //     dbExample
+  //   ) {
+  //     res.render("example", {
+  //       example: dbExample
+  //     });
+  //   });
+  // });
 
   // Render 404 page for any unmatched routes
   app.get("*", function(req, res) {
