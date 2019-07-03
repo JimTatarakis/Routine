0377d41fdf176e9840e35e00a1eabaecf83bf542
Justin Raithel <jeraithel@yahoo.com>
2019-07-02 20:34:20 -0700

    working split user/manager create/view

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -7,44 +7,103 @@ var db = require("../models");
 // User Routes
 // =============================================================
 module.exports = (app) => {
-    // GET route : find all users
-    app.get("/api/users/all", function (req, res) {
-        db.User.findAll({}).then(function (answer) {
-            res.json(answer);
-        });
+  // GET route : find all users
+  app.get("/api/users/all", function (req, res) {
+    db.User.findAll({}).then(function (answer) {
+      res.json(answer);
     });
+  });
 
-    // GET route : find user with id and associated tasks
-    app.get("/api/users/:id", function (req, res) {
-        db.User.findAll({ 
-            where: { id: req.params.id },
-            include: [db.Task]
-        })
+  // GET route : find user with id and associated tasks
+  app.get("/api/users/:id", function (req, res) {
+    db.User.findAll({
+      where: { id: req.params.id },
+      include: [db.Task]
+    })
+      .then(function (answer) {
+        res.json(answer);
+      });
+  });
+
+  // POST route : add User
+  app.post("/api/users/register", function (req, res) {
+    if (req.body.secret === "cats") {
+      db.User.create({
+        username: req.body.username,
+        password: req.body.password,
+        displayName: req.body.displayName,
+        email: req.body.email,
+        createdAt: Date.now(),
+        updatedAt: Date.now(),
+        manager: true
+      })
         .then(function (answer) {
-            res.json(answer);
-        });
-    });
-
-    // POST route : add User
-    app.post("/api/users/register", function (req, res) {
-        db.User.create({
-            username: req.body.username,
-            password: req.body.password,
-            displayName: req.body.displayName,
-            email: req.body.email,
-            createdAt: Date.now(),
-            updatedAt: Date.now(),
-            manager: null
+          var message = "Manager Created. Please Login";
+          res.render("login", { Message: message });
         })
-            .then(function (answer) {
-                res.json(answer);
-            })
-            .catch(function (err) {
-                console.log(err);
-            });
-    });
+        .catch(function (err) {
+          console.log(err);
+        });
+    } else {
+      db.User.create({
+        username: req.body.username,
+        password: req.body.password,
+        displayName: req.body.displayName,
+        email: req.body.email,
+        createdAt: Date.now(),
+        updatedAt: Date.now(),
+        manager: false
+      })
+        .then(function (answer) {
+          var message = "User Created. Please Login";
+          res.render("login", { Message: message });
+        })
+        .catch(function (err) {
+          console.log(err);
+        });
+    }
+  });
 
-      // DELETE route : user delete
+  // POST route : add User from Manager screen
+  app.post("/api/users/register2", function (req, res) {
+    if (req.body.secret === "cats") {
+      db.User.create({
+        username: req.body.username,
+        password: req.body.password,
+        displayName: req.body.displayName,
+        email: req.body.email,
+        createdAt: Date.now(),
+        updatedAt: Date.now(),
+        manager: true
+      })
+        .then(function () {
+          var message = "Manager Created. Please Login";
+          res.render("login", { Message: message });
+        })
+        .catch(function (err) {
+          console.log(err);
+        });
+    } else {
+      db.User.create({
+        username: req.body.username,
+        password: req.body.password,
+        displayName: req.body.displayName,
+        email: req.body.email,
+        createdAt: Date.now(),
+        updatedAt: Date.now(),
+        manager: false
+      })
+        .then(function () {
+          var message = "User Created. Please Login";
+          res.render("login", { Message: message });
+        })
+        .catch(function (err) {
+          console.log(err);
+        });
+    }
+  });
+
+  // DELETE route : user delete
   app.delete("/api/users/:id", function (req, res) {
     db.User.destroy({
       where: {
